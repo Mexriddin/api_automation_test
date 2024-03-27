9798aa4a9e8b9572c9e0b5e805059662cf4bcc31
Mexriddin Maxkamtayev <maxkamtayev@gmail.com>
2024-03-27 14:53:57 +0500

    add error model

--- services/users/models/user_model.py
+++ services/users/models/user_model.py
@@ -31,3 +31,15 @@ class Meta(BaseModel):
 class UsersModel(BaseModel):
     items: List[UserModel]
     meta: Meta
+
+
+class ErrorModel(BaseModel):
+    message: str
+    code: int
+
+    @field_validator("code", "message")
+    def field_are_not_empty(cls, value):
+        if value == "" or value is None:
+            raise ValueError("Field must not be empty")
+        else:
+            return value
