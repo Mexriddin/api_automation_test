6ba13ddbf0285f7a8996afce33031110a2774b21
Mexriddin Maxkamtayev <maxkamtayev@gmail.com>
2024-05-29 17:00:52 +0500

    install email validators

--- services/games/models/game_models.py
+++ services/games/models/game_models.py
@@ -6,7 +6,7 @@ from services.categories.models.category_models import CategoryModel
 
 
 class GameModel(BaseModel):
-    category_uuids: List[CategoryModel.uuid]
+    category_uuids: List[str]
     price: int = Field(..., ge=0)
     title: str = Field(..., max_length=100)
     uuid: str = Field(..., min_length=36, max_length=36,
